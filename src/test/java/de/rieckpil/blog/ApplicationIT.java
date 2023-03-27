84af1415f599f67b1b12a81cf9865f826e3d0264
alex-latitude <alex-latitude@laposte.net>
2023-03-27 12:55:06 +0200

    wip

--- src/test/java/de/rieckpil/blog/ApplicationIT.java
+++ src/test/java/de/rieckpil/blog/ApplicationIT.java
@@ -1,6 +1,8 @@
 package de.rieckpil.blog;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -9,6 +11,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 // JUnit 5 example with Spring Boot >= 2.2.6
@@ -16,20 +22,49 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ApplicationIT {
 
-  @Container
-  public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
-    .withPassword("inmemory")
-    .withUsername("inmemory");
+    private final Logger logger = LoggerFactory.getLogger(ApplicationIT.class);
 
-  @DynamicPropertySource
-  static void postgresqlProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-    registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-    registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-  }
+    @Container
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
+            .withPassword("inmemory")
+            .withUsername("inmemory")
+            .withDatabaseName("test-database");
 
-  @Test
-  public void contextLoads() {
-  }
+    // TODO à quoi ça sert ça ?
+    //            .withCommand("top");
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+    }
+
+    @Test
+    public void contextLoads() {
+        assertTrue(postgreSQLContainer.isRunning());
+        logger.info("containerId de mon container: " + postgreSQLContainer.getContainerId());
+
+        try {
+            org.testcontainers.containers.Container.ExecResult result = postgreSQLContainer.execInContainer("ls", "-al");
+            logger.info("======================================================================================= result.toString()");
+            logger.info(result.toString());
+            logger.info("======================================================================================= result.getStdout()");
+            logger.info(result.getStdout());
+            logger.info("======================================================================================= result.getStdout()");
+
+            org.testcontainers.containers.Container.ExecResult env = postgreSQLContainer.execInContainer("env");
+            logger.info("======================================================================================= env");
+            logger.info(env.getStdout());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        logger.info(postgreSQLContainer.getJdbcUrl());
+        logger.info(postgreSQLContainer.getUsername());
+        logger.info(postgreSQLContainer.getPassword());
+    }
 
 }
